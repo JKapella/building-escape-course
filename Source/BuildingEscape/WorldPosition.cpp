91ab56c58e31049a4ac79ecfc2d47ef5d094bb35
James <jvkapella@gmail.com>
2020-07-14 17:08:28 +0100

    Testing GetOwner and Get Name and logging to console

--- Source/BuildingEscape/WorldPosition.cpp
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,10 +20,9 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("WARNING!"));
-	UE_LOG(LogTemp, Error, TEXT("ERROR!"));
-	UE_LOG(LogTemp, Display, TEXT("A Nice Message"));
-	
+	FString ActorName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("World Pos Component, actor name is: %s"), *ActorName);
 }
 
 
